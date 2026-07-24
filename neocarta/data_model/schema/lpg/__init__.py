6b5028f3c6bb508329cd6803e70581a9e26647d8
David DeBruce <david.debruce@neo4j.com>
2026-07-24 12:04:37 -0400

    chore(lpg): drop in-progress warning now that LPG has a consumer

--- neocarta/data_model/schema/lpg/__init__.py
+++ neocarta/data_model/schema/lpg/__init__.py
@@ -1,7 +1,5 @@
 """LPG (Labeled Property Graph) structural data model nodes and relationships."""
 
-import warnings
-
 from .models import (
     Database,
     HasNode,
@@ -15,11 +13,6 @@ from .models import (
     Relationship,
     RelationshipHasProperty,
     Schema,
-)
-
-warnings.warn(
-    "LPG data model components are an in-progress feature. There is no application in the current library version.",
-    stacklevel=2,
 )
 
 __all__ = [
